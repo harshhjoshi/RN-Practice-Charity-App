e5f0e4e59da9ea14a60c7a79d8d8882d833d9be2
deep9824 <deep.bhatt@reynasolutions.com>
2023-08-29 14:30:11 +0530

    design

--- source/components/yellowbtn.tsx
+++ source/components/yellowbtn.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import LinearGradient from 'react-native-linear-gradient';
-import {TouchableOpacity, Text, ActivityIndicator} from 'react-native';
-import {styles} from '../screens/Login/Loginstyles';
-import { colors, responsiveHeight, responsiveWidth } from '../styles/variables';
+import React from "react";
+import LinearGradient from "react-native-linear-gradient";
+import { TouchableOpacity, Text, ActivityIndicator } from "react-native";
+import { styles } from "../screens/Login/Loginstyles";
+import { colors, responsiveHeight, responsiveWidth } from "../styles/variables";
 
-const YellowBtn = ({ margintop,text, onPress,needLoading}: any) => {
+const YellowBtn = ({ margintop, text, onPress, needLoading }: any) => {
   return (
     <TouchableOpacity
-    style={{backgroundColor:'#E0A14E',width:responsiveWidth(90),borderRadius:40,height:responsiveHeight(7),alignSelf:'center',marginBottom:10,
-    justifyContent: 'center',marginTop:margintop
-}}
-    
-    onPress={onPress} activeOpacity={0.8}>
-          {needLoading?(
-          <ActivityIndicator size="small" color={colors.white} />
-        ) : (
+      style={{
+        backgroundColor: "#E0A14E",
+        width: responsiveWidth(90),
+        borderRadius: 40,
+        height: responsiveHeight(7),
+        alignSelf: "center",
+        marginBottom: 10,
+        justifyContent: "center",
+        marginTop: margintop,
+      }}
+      onPress={onPress}
+      activeOpacity={0.8}
+    >
+      {needLoading ? (
+        <ActivityIndicator size="small" color={colors.white} />
+      ) : (
         <Text style={styles.buttonText}>{text}</Text>
-        )}
+      )}
     </TouchableOpacity>
   );
 };
